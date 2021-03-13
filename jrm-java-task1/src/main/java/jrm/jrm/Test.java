c4c1dff074aadab3668f6f7aeccf4e3668fc6045
misssLuv <misssLuv@yandex.ru>
2021-03-14 00:42:12 +0300

    task1 solution and test cases

---
+++ jrm-java-task1/src/main/java/jrm/jrm/Test.java
@@ -0,0 +1,6 @@
+//dc89aba4-0270-4269-9f94-caa11113719b
+package jrm;
+
+public class Test {
+
+}
---
+++ jrm-java-task2/src/main/java/jrm/ShowAllPrimitiveDataTypes.java
@@ -0,0 +1,6 @@
+// f3562616-aefb-44a7-84ee-8a21a6d487a6
+
+
+
+public class ShowAllPrimitiveDataTypes {
+}
